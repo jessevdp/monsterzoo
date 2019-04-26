f6e9359e36e50e2895256252f7a4a0f17f690bc9
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-04-26 16:12:18 +0200

    Setup an example template situation in index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-document.title = process.env.APP_NAME;
-document.getElementById('title').innerText = process.env.APP_NAME;
+import { renderTemplate } from '@local/utilities';
+import template from './index.template.html';
+
+const APP_NAME = process.env.APP_NAME;
+document.title = APP_NAME;
+document.body.innerHTML = renderTemplate(template, { title: APP_NAME });
