05a932386cb7838a148726bddd3210a2e35a8978
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-04-27 11:28:12 +0200

    :white_check_mark: Add test for type of data param in renderTemplate function

--- src/utilities/template.spec.js
+++ src/utilities/template.spec.js
@@ -16,28 +16,41 @@ describe('renderTemplate', () => {
         // Assert
         expect(result).toBe(expected);
     })
+    
     it('uses mustache.render', () => {
         const template = "template string"
         const data = { foo: 'bar' };
         renderTemplate(template, data);
         expect(mustache.render).toHaveBeenCalledWith(template, data);
     })
+
     it('defaults [data] param to an empty object', () => {
         const template = "template string"
         renderTemplate(template);
         expect(mustache.render).toHaveBeenCalledWith(template, {});
     })
+
     it('throws without [template] param', () => {
         expect(() => renderTemplate()).toThrow()
     })
+
     test.each([
         [[]],
         [{}],
         [false],
         [10],
-        [null],
-        [undefined]
+        [null]
     ])('throws when [template] param is not a string (%#)', (param) => {
         expect(() => renderTemplate(param)).toThrow()
+    })
+
+    test.each([
+        [[]],
+        ['string'],
+        [false],
+        [10],
+        [null]
+    ])('throws when [data] param is not an object (%#)', (param) => {
+        expect(() => renderTemplate('template', param)).toThrow()
     })
 })
