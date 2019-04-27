4a6188fe485f07fdfcf195ae1646ca5853b83160
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-04-27 11:10:11 +0200

    :white_check_mark: Add tests for renderTemplate utility

---
+++ src/utilities/template.spec.js
@@ -0,0 +1,43 @@
+import { renderTemplate } from './template';
+import mustache from 'mustache';
+
+jest.mock('mustache');
+
+describe('renderTemplate', () => {
+    it('can render a template with data', () => {
+        // Arrange
+        const template = '<h1>{{ foo }} </h1>';
+        const data = { foo: 'bar' };
+        const expected = mustache.render(template, data);
+
+        // Act
+        const result = renderTemplate(template, data);
+
+        // Assert
+        expect(result).toBe(expected);
+    })
+    it('uses mustache.render', () => {
+        const template = "template string"
+        const data = { foo: 'bar' };
+        renderTemplate(template, data);
+        expect(mustache.render).toHaveBeenCalledWith(template, data);
+    })
+    it('defaults [data] param to an empty object', () => {
+        const template = "template string"
+        renderTemplate(template);
+        expect(mustache.render).toHaveBeenCalledWith(template, {});
+    })
+    it('throws without [template] param', () => {
+        expect(() => renderTemplate()).toThrow()
+    })
+    test.each([
+        [[]],
+        [{}],
+        [false],
+        [10],
+        [null],
+        [undefined]
+    ])('throws when [template] param is not a string (%#)', (param) => {
+        expect(() => renderTemplate(param)).toThrow()
+    })
+})
