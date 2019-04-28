8a2073971a1a3d386afd35d6acbff2e15e375792
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-04-28 17:10:26 +0200

    :white_check_mark: Add tests for isArray typecheck utility

--- src/utilities/typecheck.spec.js
+++ src/utilities/typecheck.spec.js
@@ -1,4 +1,7 @@
-import { isObject } from './typecheck';
+import { 
+    isObject,
+    isArray,
+} from './typecheck';
 
 describe('isObject', () => {
     it(`returns 'true' for objects`, () => {
@@ -14,6 +17,24 @@ describe('isObject', () => {
         [null]
     ])(`returns 'false' for non-objects (%#)`, (param) => {
         const result = isObject(param);
+        expect(result).toBeFalsy();
+    })
+})
+
+describe('isArray', () => {
+    it(`returns 'true' for arrays`, () => {
+        const object = [];
+        const result = isArray(object);
+        expect(result).toBeTruthy();
+    })
+    test.each([
+        [{}],
+        ['string'],
+        [10],
+        [false],
+        [null]
+    ])(`returns 'false' for other types (%#)`, (param) => {
+        const result = isArray(param);
         expect(result).toBeFalsy();
     })
 })
