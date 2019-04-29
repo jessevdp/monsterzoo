7bf1da4d20becd2b9134b314a2b5bf6f628e9a5c
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-04-29 19:44:02 +0200

    Implement Component.setState

--- src/system/component.js
+++ src/system/component.js
@@ -1,8 +1,12 @@
-import { isFunction, isString } from '@local/utilities';
+import {
+    isFunction,
+    isString,
+    isObject 
+} from '@local/utilities';
 
 export default class Component {
     /**
-     *Creates an instance of Component.
+     * Creates an instance of Component.
      * @memberof Component
      */
     constructor() {
@@ -20,5 +24,20 @@ export default class Component {
         const view  = this.view();
         if (!isString(view)) throw new Error('Component [view] function must return a string.');
         return view;
+    }
+
+    /**
+     * Mutate the state of the component
+     *
+     * @param {*} state the subset of the state to be mutated
+     * @returns {void}
+     * @memberof Component
+     */
+    setState(state) {
+        let newState;
+        if (isObject(state)) newState = state;
+        else if (isFunction(state)) newState = state(this.state);
+        else throw new Error('Invalid [state] param, expected an object or a function');
+        this.state = { ...this.state, ...newState };
     }
 }
