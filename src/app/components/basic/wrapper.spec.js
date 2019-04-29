482a9e9952a84d1cd8e749825f254a0857d19416
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-04-29 20:32:03 +0200

    :white_check_mark: Add tests for new Wrapper component

---
+++ src/app/components/basic/wrapper.spec.js
@@ -0,0 +1,40 @@
+import { Component } from '@local/system'
+import Wrapper from './wrapper';
+
+describe('constructor', () => {
+    it('stores all components in state', () => {
+        const components = [new MockComponent(), new MockComponent(), new MockComponent()];
+        const wrapper = new Wrapper(...components);
+        expect(wrapper.state.components).toEqual(components);
+    })
+    test.each([
+        [{ foo: 'bar' }],
+        ['string'],
+        [10],
+        [false],
+        [{}, new Component()]
+    ])('only accepts instances of Component (%#)', (param) => {
+        expect(() => new Wrapper(param)).toThrow();
+    })
+})
+
+describe('render', () => {
+    it('calls render on all components', () => {
+        const components = [new MockComponent(), new MockComponent(), new MockComponent()];
+        const wrapper = new Wrapper(...components);
+        wrapper.render();
+        components.forEach(component => expect(component.render).toHaveBeenCalled());
+    })
+    it('returns all components wrapped in a <div>', () => {
+        const components = [new MockComponent('1'), new MockComponent('2'), new MockComponent('3')];
+        const wrapper = new Wrapper(...components);
+        const result = wrapper.render();
+        expect(result).toBe('<div> 123 </div>');
+    })
+})
+
+function MockComponent(view = '') {
+    this.render = jest.fn(() => view);
+    this.view = jest.fn(() => view);
+}
+MockComponent.prototype = Object.create(Component.prototype);
