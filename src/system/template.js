d8d83b3e79f28479c127a23ca5184b3f9d2da20a
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-04-29 21:58:40 +0200

    Implement rendering of components in template

--- src/system/template.js
+++ src/system/template.js
@@ -1,5 +1,6 @@
 import mustache from 'mustache';
-import { isObject } from '@local/utilities';
+import { isObject, isArray } from '@local/utilities';
+import Component from './component';
 
 /**
  * Render a template string with a given data object.
@@ -12,5 +13,37 @@ import { isObject } from '@local/utilities';
 export function renderTemplate (template, data = {}) {
     if (typeof template !== 'string') throw new Error('Invalid [template] parameter, expected a string.');
     if (!isObject(data)) throw new Error('Invalid [data] parameter, expected an object.');
-    return mustache.render(template, data);
+    // data = renderComponentsInObject(data);
+    return mustache.render(template, renderComponentsInObject(data));
+}
+
+/**
+ * Replaces instances of Component with their rendered version
+ *
+ * @param {object} object
+ * @returns {object}
+ */
+function renderComponentsInObject(object) {
+    const newObject = {};
+    for (let key in object) {
+        const current = object[key];
+        if (current instanceof Component) newObject[key] = current.render();
+        else if (isObject(current)) newObject[key] = renderComponentsInObject(current);
+        else if (isArray(current)) newObject[key] = renderComponentsInArray(current);
+        else newObject[key] = object[key];
+    }
+    return newObject;
+}
+
+/**
+ * Replaces instances of Component with their rendered version
+ *
+ * @param {array} array
+ * @returns {array}
+ */
+function renderComponentsInArray(array) {
+    return array.map(item => {
+        if (item instanceof Component) return item.render();
+        else return item;
+    });
 }
