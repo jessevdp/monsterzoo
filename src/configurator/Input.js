7d146073152dd1c916eefe63ebe55cedc84dd4ce
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-05-23 15:04:47 +0200

    :sparkles: Implement Input component constructor

--- src/configurator/Input.js
+++ src/configurator/Input.js
@@ -1,3 +1,22 @@
 import { Component } from '@local/system';
+
+const defaultAttributes = {
+    'type': 'text'
+};
+
 export default class Input extends Component {
+    /**
+     * Creates an instance of Input.
+     * @param {string} name
+     * @param {object} [attributes={}] A set of attributes for the input box, represented as key-value pairs
+     * @memberof Input
+     */
+    constructor(name, attributes = {}) {
+        super();
+        attributes = {...defaultAttributes, ...attributes};
+        this.setState({
+            name,
+            attributes
+        });
+    }
 }
