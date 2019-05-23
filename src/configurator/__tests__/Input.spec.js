228bb7868ea389e944d2d48f8c5474b85687b0a6
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-05-23 15:22:00 +0200

    :white_check_mark: Add tests for Input.setAttributes

--- src/configurator/__tests__/Input.spec.js
+++ src/configurator/__tests__/Input.spec.js
@@ -34,3 +34,55 @@ describe('htmlAttributes', () => {
         input.cleanup();
     })
 })
+
+describe('setAttributes', () => {
+    it('accepts an object to update the attributes', () => {
+        const input = new Input('name');
+        const attributes = { foo: 'bar' };
+        input.setAttributes(attributes);
+        expect(input.state.attributes.foo).toBe(attributes.foo);
+        input.cleanup();
+    })
+    it('accepts a function to update the attributes', () => {
+        const input = new Input('name');
+        const attributes = { foo: 'bar' };
+        input.setAttributes(() => attributes);
+        expect(input.state.attributes.foo).toBe(attributes.foo);
+        input.cleanup();
+    })
+    it('passes the old attributes to the passed function', () => {
+        // Arrange
+        const oldAttributes = { foo: 'foo' };
+        const newAttributes = { foo: 'bar' };
+        const fn = jest.fn(() => newAttributes);
+        const input = new Input('name');
+        input.state.attributes = oldAttributes;
+
+        // Act
+        input.setAttributes(fn);
+
+        // Assert
+        expect(fn).toHaveBeenCalledWith(oldAttributes);
+
+        // Cleanup
+        input.cleanup();
+    })
+    it('overwrites old values', () => {
+        const oldAttributes = { foo: 'foo' };
+        const newAttributes = { foo: 'bar' };
+        const input = new Input('name', oldAttributes);
+        input.setAttributes(newAttributes);
+        expect(input.state.attributes.foo).toBe(newAttributes.foo);
+        input.cleanup();
+    })
+    it('leaves other values intact', () => {
+        const input = new Input('name');
+        const fooAttribute = { foo: 'foo' };
+        const barAttribute = { bar: 'bar' };
+        input.setAttributes(fooAttribute);
+        input.setAttributes(barAttribute);
+        expect(input.state.attributes.foo).toBe(fooAttribute.foo);
+        expect(input.state.attributes.bar).toBe(barAttribute.bar);
+        input.cleanup();
+    })
+})
