479286c461deb63e5a9aac0266421f4361d99043
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-05-27 17:36:21 +0200

    :white_check_mark: Add tests for 2way data binding of selected value for Select component

--- src/configurator/__tests__/Select.spec.js
+++ src/configurator/__tests__/Select.spec.js
@@ -1,5 +1,9 @@
 import Select from '../Select';
 
+beforeEach(() => {
+    document.body.innerHTML = '';
+});
+
 it('renders correctly', () => {
     const options = ['foo', 'bar'];
     const select = new Select('name', 'label', options);
@@ -51,6 +55,68 @@ describe('[options] getter', () => {
         const options = ['foo', 'bar'];
         const select = new Select('name', 'label', options);
         expect(select.options).toBe(options);
+        select.cleanup();
+    })
+})
+
+describe('[value] setter', () => {
+    it('updates the [value] variable on state', () => {
+        const options = ['foo', 'bar'];
+        const newValue = options[1];
+        const select = new Select('name', 'label', options);
+        select.value = newValue;
+        expect(select.state.value).toBe(newValue);
+        select.cleanup();
+    })
+    it('updates the selected value of the input box', () => {
+        const options = ['foo', 'bar'];
+        const newValue = options[1];
+        const select = new Select('name', 'label', options);
+        document.body.innerHTML = select.render();
+        select.value = newValue;
+        const $select = document.querySelector('select');
+        expect($select.value).toBe(newValue);
+        select.cleanup();
+    })
+    it('uses the setState method', () => {
+        const options = ['foo', 'bar'];
+        const value = options[1];
+        const select = new Select('name', 'label', options);
+        const spy = jest.spyOn(Select.prototype, 'setState');
+        select.value = value;
+        expect(spy).toHaveBeenCalledWith({ value: value });
+        select.cleanup();
+    })
+})
+
+describe('[value] getter', () => {
+    it('returns the value of state.value', () => {
+        const options = ['foo', 'bar'];
+        const value = options[1];
+        const select = new Select('name', 'label');
+        select.state.value = value;
+        expect(select.value).toBe(value);
+        select.cleanup();
+    })
+})
+
+describe('events', () => {
+    it('updates [value] state when the value of the select box changes', () => {
+        // Arrange
+        const options = ['foo', 'bar'];
+        const newValue = options[1];
+        const select = new Select('name', 'label', options);
+        document.body.innerHTML = select.render();
+        const $select = document.querySelector('select');
+
+        // Act
+        $select.value = newValue;
+        $select.dispatchEvent(new Event('change'));
+
+        // Assert
+        expect(select.state.value).toBe(newValue);
+
+        // Cleanup
         select.cleanup();
     })
 })
