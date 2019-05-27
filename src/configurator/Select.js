0d19c4be29fadd3d1fba2899e5f64af50d557d87
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-05-27 17:46:13 +0200

    :sparkles: Implement two-way data binding for Select.value

--- src/configurator/Select.js
+++ src/configurator/Select.js
@@ -16,11 +16,35 @@ export default class Select extends Component {
                 + '<label>{{label}}</label>'
                 + '<select name="{{name}}">'
                     + '{{#options}}'
-                        + '<option value="{{.}}">{{.}}</option>'
+                        + '{{{.}}}'
                     + '{{/options}}'
                 + '</select>'
             + '</div>';
-        return renderTemplate(template, this.state);
+
+        const options = this.state.options.map(option => {
+            const selected = this.state.value === option ? ' selected' : '';
+            return `<option value="${option}"${selected}>${option}</option>`;
+        });
+
+        return renderTemplate(template, {...this.state, options});
+    }
+
+    events() {
+        this.on('change', 'select', () => {
+            const $select = this.getHTMLElement('select');
+            const isActive = document.activeElement === $select;
+            this.setState({ value: $select.value });
+            if (isActive) this.getHTMLElement('select').focus();
+        });
+    }
+
+
+    set value(value) {
+        this.setState({ value });
+    }
+
+    get value() {
+        return this.state.value;
     }
 
     set options(options) {
