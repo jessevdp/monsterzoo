55464b51aada660e74ea238609ab3e585520914a
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-05-28 15:25:23 +0200

    Initialize all inputs for the Configurator and add them to the layout/template

--- src/configurator/Configurator.js
+++ src/configurator/Configurator.js
@@ -1,9 +1,34 @@
 import { Component, renderTemplate } from '@local/system';
+import Input from './Input';
+import Select from './Select';
+
 import template from './Configurator.template.html';
 import './styles/Configurator.scss';
 
 export default class Configurator extends Component {
+    constructor() {
+        super();
+
+        this.inputs = {
+            name: new Input('name', 'Name'),
+            type: new Select('type', 'Type', ['water', 'fire', 'earth', 'wind']),
+            strength: new Input('strenght', 'Strenght', { type: 'number', min: 0, max: 10, step: 1 }),
+            skintype: new Select('skintype', 'Skin type', ['scales', 'slime']),
+            skincolor: new Select('skincolor', 'Skin color', ['blue', 'red', 'green']),
+            armtype: new Select('armtype', 'Arm type', ['tentacles', 'fins']),
+            arms: new Input('arms', 'Amount of arms', { type: 'number', min: 0, max: 8, step: 1 }),
+            eyes: new Input('eyes', 'Amount of eyes', { type: 'number', min: 0, max: 8, step: 1 }),
+            legs: new Input('legs', 'Amount of legs', { type: 'number', min: 0, max: 4, step: 1 }),
+        }
+    }
+
     view() {
-        return renderTemplate(template);
+        const inputs = Object.values(this.inputs);
+        return renderTemplate(template, { inputs });
+    }
+
+    cleanup() {
+        super.cleanup();
+        Object.values(this.inputs).forEach(input => input.cleanup());
     }
 }
