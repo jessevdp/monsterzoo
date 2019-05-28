91d2638bd1f7075b8a8e07b1e619e58c644ec421
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-05-28 20:06:50 +0200

    Implement NumberInput.setAttributes

--- src/configurator/NumberInput.js
+++ src/configurator/NumberInput.js
@@ -6,6 +6,14 @@ export default class NumberInput extends Input {
         super(name, label, {...attributes, type: 'number'});
     }
 
+    setAttributes(attributes) {
+        const min = attributes.min;
+        const max = attributes.max;
+        if (isNumber(min) && min > this.value) this.value = min;
+        if (isNumber(max) && max < this.value) this.value = max;
+        super.setState(attributes);
+    }
+
     set value(value) {
         const min = this.state.attributes.min;
         const max = this.state.attributes.max;
