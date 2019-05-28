ac0e18830c1b9043afc50dbe8fac2c8e92053118
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-05-28 16:56:31 +0200

    Work the new options ruleset into the Configurator

--- src/configurator/Configurator.js
+++ src/configurator/Configurator.js
@@ -1,6 +1,7 @@
 import { Component, renderTemplate } from '@local/system';
 import Input from './Input';
 import Select from './Select';
+import options from './options';
 
 import template from './Configurator.template.html';
 import './styles/Configurator.scss';
@@ -11,15 +12,19 @@ export default class Configurator extends Component {
 
         this.inputs = {
             name: new Input('name', 'Name'),
-            type: new Select('type', 'Type', ['water', 'fire', 'earth', 'wind']),
-            strength: new Input('strenght', 'Strenght', { type: 'number', min: 0, max: 10, step: 1 }),
-            skintype: new Select('skintype', 'Skin type', ['scales', 'slime']),
-            skincolor: new Select('skincolor', 'Skin color', ['blue', 'red', 'green']),
-            armtype: new Select('armtype', 'Arm type', ['tentacles', 'fins']),
-            arms: new Input('arms', 'Amount of arms', { type: 'number', min: 0, max: 8, step: 1 }),
-            eyes: new Input('eyes', 'Amount of eyes', { type: 'number', min: 0, max: 8, step: 1 }),
-            legs: new Input('legs', 'Amount of legs', { type: 'number', min: 0, max: 4, step: 1 }),
+            type: new Select('type', 'Type', options.types),
+            strength: new Input('strenght', 'Strenght', { type: 'number' }),
+            skinType: new Select('skintype', 'Skin type'),
+            skinColor: new Select('skincolor', 'Skin color'),
+            armType: new Select('armtype', 'Arm type'),
+            arms: new Input('arms', 'Amount of arms', { type: 'number' }),
+            eyes: new Input('eyes', 'Amount of eyes', { type: 'number' }),
+            legs: new Input('legs', 'Amount of legs', { type: 'number' }),
         }
+        
+        bindInputs(this);
+        this.setState({ name: '', ...options.defaults });
+        this.configureInputs();
     }
 
     view() {
@@ -27,8 +32,39 @@ export default class Configurator extends Component {
         return renderTemplate(template, { inputs });
     }
 
+    configureInputs() {
+        this.inputs.strength.setAttributes(this.options.strength());
+        this.inputs.skinType.options = this.options.skinTypes();
+        this.inputs.skinColor.options = this.options.skinColors();
+        this.inputs.armType.options = this.options.armTypes();
+        this.inputs.arms.setAttributes(this.options.armCount());
+        this.inputs.eyes.setAttributes(this.options.eyeCount());
+        this.inputs.legs.setAttributes(this.options.legCount());
+    }
+
+    setState(...args) {
+        super.setState(...args);
+        this.configureInputs();
+    }
+
+    get options() {
+        return options.for(this.state.type);
+    }
+
     cleanup() {
         super.cleanup();
         Object.values(this.inputs).forEach(input => input.cleanup());
     }
+}
+
+function bindInputs(configurator) {
+    configurator.bind('name', configurator.inputs.name, 'value');
+    configurator.bind('type', configurator.inputs.type, 'value');
+    configurator.bind('strength', configurator.inputs.strength, 'value');
+    configurator.bind('skinType', configurator.inputs.skinType, 'value');
+    configurator.bind('skinColor', configurator.inputs.skinColor, 'value');
+    configurator.bind('armType', configurator.inputs.armType, 'value');
+    configurator.bind('arms', configurator.inputs.arms, 'value');
+    configurator.bind('eyes', configurator.inputs.eyes, 'value');
+    configurator.bind('legs', configurator.inputs.legs, 'value');
 }
