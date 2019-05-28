6fccc504a787768d5ee76d939eedfd98a140d779
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-05-28 19:54:25 +0200

    Implement NumberInput value getter/setter

--- src/configurator/NumberInput.js
+++ src/configurator/NumberInput.js
@@ -1,7 +1,20 @@
 import Input from './Input';
+import { isNumber } from '@local/utilities';
 
 export default class NumberInput extends Input {
     constructor (name, label, attributes = {}) {
         super(name, label, {...attributes, type: 'number'});
+    }
+
+    set value(value) {
+        const min = this.state.attributes.min;
+        const max = this.state.attributes.max;
+        if (isNumber(min) && value < min) value = min;
+        if (isNumber(max) && value > max) value = max;
+        super.value = value;
+    }
+
+    get value() {
+        return Number(super.value);
     }
 }
