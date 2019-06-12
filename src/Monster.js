848f079f98b187956ddee0a6117340460b745368
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-06-12 08:32:13 +0200

    Create a first version of the popover for Monster component stats

--- src/Monster.js
+++ src/Monster.js
@@ -1,4 +1,5 @@
 import { Component, renderTemplate } from '@local/system';
+import Tooltip from 'tooltip.js';
 import Registry from './Registry';
 import './Monster.scss';
 
@@ -16,13 +17,47 @@ export default class Monster extends Component {
         return renderTemplate(template, { color: this.state.skinColor });
     }
 
+    statsView() {
+        const template = ''
+            + '<div class="bg-800 monster--stats">'
+                + '<p class="text-sm stats--description">{{ description }}</p>'
+            + '</div>';
+        const description = this.getStatsMessage();
+        return renderTemplate(template, { description });
+    }
+
     events() {
         this.on('dragstart', e => {
             const index = Registry.add(this);
             e.dataTransfer.setData('monster', index);
         });
-
         this.on('dragstart', () => this.getHTMLElement().classList.add('dragged'));
         this.on('dragend', () => this.getHTMLElement().classList.remove('dragged'));
     }
+
+    effects(useEffect) {
+        useEffect(() => {
+            const $el = this.getHTMLElement();
+            const options = {
+                title: this.statsView(),
+                html: true,
+                placement: 'right',
+                trigger: 'hover',
+            };
+            const tooltip = new Tooltip($el, options);
+            return () => tooltip.dispose();
+        });
+    }
+
+    getStatsMessage() {
+        const skin = `${this.state.skinColor} ${this.state.skinType}`;
+        const arms = `${number(this.state.arms)} ${this.state.armType}`;
+        const legs = `${number(this.state.legs)} legs`;
+        const eyes = `${number(this.state.eyes)} eyes`;
+        return `${this.state.name} is a ${this.state.type} monster with ${skin}, ${arms}, ${legs}, and ${eyes}.`;
+    }
+}
+
+function number(n) {
+    return (n === 0) ? 'no' : n;
 }
