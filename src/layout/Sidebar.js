c4eb9d123025410c5c1d7c4de5583122e85f7290
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-06-13 19:57:23 +0200

    Introduce a Sidebar component which contains the Configurator

---
+++ src/layout/Sidebar.js
@@ -0,0 +1,16 @@
+import { Component, renderTemplate } from '@local/system';
+import Configurator from '@local/configurator/Configurator';
+
+export default class Sidebar extends Component {
+    constructor() {
+        super();
+        this.setState({
+            configurator: new Configurator(),
+        });
+    }
+
+    view() {
+        const template = '<div class="sidebar">{{{ configurator }}}</div>';
+        return renderTemplate(template, this.state);
+    }
+}
