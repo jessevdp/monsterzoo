6f74082a701ee06a5ece20fc4371f5b2c8801902
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-06-14 09:51:16 +0200

    Add a trash icon as a placeholder to Bin component

--- src/sidebar/Bin.js
+++ src/sidebar/Bin.js
@@ -1,4 +1,5 @@
 import SidebarTile from './SidebarTile';
+import './styles/Bin.scss';
 
 const title = 'Monster Bin';
 const description = 'Drag a monster here to remove it form the zoo.';
@@ -11,5 +12,14 @@ export default class Bin extends SidebarTile {
     placeMonster(monster) {
         super.placeMonster(monster);
         this.removeMonster();
+    }
+
+    get placeholder() {
+        return ''
+            + '<div class="bin--placeholder">'
+                + '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">'
+                    + '<path d="M432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16zM53.2 467a48 48 0 0 0 47.9 45h245.8a48 48 0 0 0 47.9-45L416 128H32z"/>'
+                + '</svg>'
+            + '</div>';
     }
 }
