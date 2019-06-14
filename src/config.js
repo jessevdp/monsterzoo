b3b8cba7f99c05d7f8d2817e6050a3f99de9cffe
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-06-14 16:24:57 +0200

    :memo: Add a doccumentation comment to OpenWeatherMap key in config

--- src/config.js
+++ src/config.js
@@ -4,6 +4,12 @@ export default {
     app: {
         name: process.env.APP_NAME || 'Monster Zoo',
     },
+    /*
+     * OpenWeatherMap
+     * ---------------------------
+     * The OpenWeatherMap API is used to fetch weather data for the application. You can
+     * obtain an API key (free for limited use) here: https://openweathermap.org/appid.
+     */
     openWeatherMap: {
         apiKey: process.env.OPEN_WEATHER_MAP_API_KEY
     }
