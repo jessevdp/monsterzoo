fc1a2f3d2b68972bf67357a012467ba4c21e3516
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-06-17 19:18:16 +0200

    Implement WeatherAPI.fetchWeatherForCity

--- src/common/WeatherAPI/WeatherAPI.js
+++ src/common/WeatherAPI/WeatherAPI.js
@@ -1,5 +1,16 @@
+import WeatherCondition from './WeatherCondition';
+
 export default class WeatherAPI {
     constructor(key) {
         this._key = key;
+    }
+
+    async fetchWeatherForCity(city) {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?id=${city.id}&APPID=${this._key}&units=metric`);
+        const data = await response.json();
+        return {
+            temperature: data.main.temp,
+            condition: WeatherCondition.fromID(data.weather[0].id),
+        };
     }
 }
