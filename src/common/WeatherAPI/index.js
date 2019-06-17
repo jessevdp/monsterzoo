4a184fb64eccd57b341fe306915cfe0e7c571f9a
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-06-17 19:16:25 +0200

    Setup an enum of possible reference cities for the WeatherAPI

--- src/common/WeatherAPI/index.js
+++ src/common/WeatherAPI/index.js
@@ -1,8 +1,11 @@
 import config from '@local/config';
 import WeatherAPI from './WeatherAPI';
+import ReferenceCity from './ReferenceCity';
 
 const key = config.openWeatherMap.apiKey;
 if (!key) throw new Error('The API key for OpenWeatherMap has not been set. (OPEN_WEATHER_MAP_API_KEY)');
 
-const weatherAPI = new WeatherAPI(key);
-export default weatherAPI;
+export default new WeatherAPI(key);
+export {
+    ReferenceCity,
+};
