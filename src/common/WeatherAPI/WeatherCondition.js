712fc50dbc14f40f84be49e23d2ae2c74e9fb23c
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-06-18 20:50:15 +0200

    Add icon for weather condition to Weather component

--- src/common/WeatherAPI/WeatherCondition.js
+++ src/common/WeatherAPI/WeatherCondition.js
@@ -2,34 +2,42 @@ import { excludeProperties } from '@local/utilities';
 
 const weatherConditions = {
     'clear': {
+        id: 1,
         name: 'clear sky',
         match: /^800$/
     },
     'fewClouds': {
+        id: 2,
         name: 'few clouds',
         match: /^801$/
     },
     'clouds': {
+        id: 3,
         name: 'clouds',
         match: /^80[2-4]$/
     },
     'rain': {
+        id: 4,
         name: 'rain',
         match: /^5[01]\d$/
     },
     'showers': {
+        id: 5,
         name: 'shower rain',
         match: /^52\d$/
     },
     'thunderstorm': {
+        id: 6,
         name: 'thunderstorm',
         match: /^2\d{2}$/
     },
     'snow': {
+        id: 7,
         name: 'snow',
         match: /^6\d{2}$/
     },
     'mist': {
+        id: 8,
         name: 'mist',
         match: /^7\d{2}$/
     },
--- src/topbar/Weather.js
+++ src/topbar/Weather.js
@@ -1,6 +1,6 @@
 import { Component, renderTemplate } from '@local/system';
 import WeatherAPI from '@local/common/WeatherAPI';
-
+import WeatherIcon from './WeatherIcon';
 import './styles/Weather.scss';
 
 export default class Weather extends Component {
@@ -10,13 +10,35 @@ export default class Weather extends Component {
     }
 
     view() {
+        const icon = this.getConditionIcon();
         const template = ''
             + '<div class="weather bg-900">'
-                + '{{#temperature}}{{.}}{{/temperature}}'
-                + '{{^temperature}}&mdash;&nbsp;{{/temperature}}'
+                + '<div class="weather--icon">'
+                    + '{{{ icon }}}'
+                + '</div>'
+                + '<span class="weather--temperature">'
+                    + '{{#temperature}}{{.}}{{/temperature}}'
+                    + '{{^temperature}}&mdash;&nbsp;{{/temperature}}'
+                + '</span>'
                 + '&deg;'
             + '</div>';
-        return renderTemplate(template, this.state);
+        return renderTemplate(template, {...this.state, icon});
+    }
+
+    getConditionIcon() {
+        if (!this.state.condition) return WeatherIcon.noData;
+
+        switch (this.state.condition.id) {
+            case 1: return WeatherIcon.sun;
+            case 2: return WeatherIcon.cloudSun;
+            case 3: return WeatherIcon.cloud;
+            case 4: return WeatherIcon.rain;
+            case 5: return WeatherIcon.showers;
+            case 6: return WeatherIcon.lightning;
+            case 7: return WeatherIcon.snow;
+            case 8: return WeatherIcon.mist;
+            default: return WeatherIcon.noData;
+        }
     }
 
     effects(useEffect) {
