77bd1b993e0b3bcadbbe6e3773c2a68250b0b6cf
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-06-19 11:10:29 +0200

    Export RadioButtonGroup from common/form

--- src/common/form/form.js
+++ src/common/form/form.js
@@ -1,11 +1,13 @@
 import Input from './Input';
 import NumberInput from './NumberInput';
 import Select from './Select';
+import RadioButtonGroup from './RadioButtonGroup';
 
 const form = {
     Input,
     NumberInput,
     Select,
+    RadioButtonGroup,
 };
 
 export default form;
