41f094c3e720f3c6b241751e4ee953d862efadbc
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-06-20 19:47:34 +0200

    Add 'id' property to Region

--- src/topbar/Region.js
+++ src/topbar/Region.js
@@ -1,11 +1,14 @@
 const regions = {
     jungle: {
+        id: 1,
         name: 'Jungle',
     },
     desert: {
+        id: 2,
         name: 'Desert',
     },
     northPole: {
+        id: 3,
         name: 'North-pole',
     }
 };
